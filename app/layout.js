28cefb92c296ec5693482bef60e31a650aaa3289
tejxsmore <tejasmore1104@gmail.com>
2023-11-24 06:44:40 +0530

    Review

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Digital Art",
+  title: "Digital Cart",
   description: "It is a NFT marketplace built using Ethereum blockchain",
 };
 
